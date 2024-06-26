dff0fe46ac185e8a2cc3687e87b7000525d218e3
Zaeem <zaeemali91@gmail.com>
2024-06-26 16:06:19 +0500

    Movie Detail page has been developed with scroll to top bug fixed.

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,5 @@
 export {Header} from './Header'
 export {Footer} from './Footer'
+
 export {Card} from './Card'
 export {Button} from './Button'
--- src/routes/AllRoutes.js
+++ src/routes/AllRoutes.js
@@ -6,11 +6,17 @@ export const AllRoutes = () => {
     <div className='dark:bg-darkbg'>
       <Routes>
         <Route path="" element={<MovieList apiPath="movie/now_playing" />} />
-        <Route path="movie/:id" element={<MovieDetail />} />
+
         <Route path="movies/popular" element={<MovieList apiPath="movie/now_playing" />} />
+        
         <Route path="movies/top" element={<MovieList apiPath="movie/top_rated" />} />
+        
         <Route path="movies/upcoming" element={<MovieList apiPath="movie/upcoming" />} />
+        
         <Route path="search" element={<Search apiPath="search/movie" />} />
+        
+        <Route path="movie/:id" element={<MovieDetail />} />
+        
         <Route path="*" element={<PageNotFound />} />
       </Routes>
     </div>
